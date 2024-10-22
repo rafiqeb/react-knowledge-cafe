f12b18b1c1324bfed327fdb9850abc3ebde00cb0
rafiqeb <mdrafiqulislameb@gmail.com>
2024-10-22 12:16:51 +0600

    first commit

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Blogs = () => {
+
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+
+    }, [])
+    return (
+        <div>
+            <h2>Blogs here</h2>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import Blogs from "../Blogs/Blogs";
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>Knowledge cafe</h1>
+            <Blogs></Blogs>
+        </div>
+    );
+};
+
+export default Header;
